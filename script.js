0ba15fe99fa5c0735a80e6bccbe22dbbc37804e9
rehmani56 <96918154+rehmani56@users.noreply.github.com>
2022-01-13 10:00:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// $('.count').counterUp({
+//     delay:10,
+//     time:3000
+// });
+function myFunction() {
+    alert("Subscribe Successfully");
+}
+// let nav_btn=document.getElementById('nav_btn');
+
+// function showmenu() {
+//     nav_btn.style.display='block';
+// }
+// function hidemenu() {
+//     nav_btn.style.display='none';
+// }
+ 
+function toogle() {
+    var x = document.getElementById('menu');
+    if (x.style.display=="block") {
+        x.style.display="none";
+    }
+    else{
+        x.style.display="block";
+    }
+}
